82594bdc30d5030551c409f8d34eb71b90bf5a0f
Фарид Курбанов <kf.dev@german-web.ru>
2026-03-14 20:35:37 +0300

    mod

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import router from './router'
 
 // 👇 1. Импортируем все иконки
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
